c998c2d60a5ae7bf1a8063a9863f0af51d8f9281
harun koyuncu <harun3851@hotmail.com>
2022-12-21 02:39:04 +0200

    style the tasks

--- js/index.js
+++ js/index.js
@@ -7,7 +7,25 @@ const getInputText = function name(params){
    return document.getElementById('task-text').value
 }
 const insertTaskIntoPage = (text) => {
-    document.getElementById('tasks-section').insertAdjacentHTML('beforeend',`<h3>${text}</h3>`)
+    document
+    .getElementById('tasks-section')
+    .insertAdjacentHTML('beforeend',`
+    <div class="card rounded-pill mt-2 text-left">
+        <div class="card-body"> 
+            <p class="card-text">${text}</p>
+            <button type="button" class="btn btn-success rounded-pill">
+                <span class="material-symbols-outlined">
+                    done
+                </span>
+            </button>
+            <button type="button" class="btn btn-danger rounded-pill">
+                <span class="material-symbols-outlined">
+                    delete
+                </span>
+            </button> 
+        </div>
+    </div>
+`)
 }
 const emptyInputText = () =>{
     document.getElementById('task-text').value=''
